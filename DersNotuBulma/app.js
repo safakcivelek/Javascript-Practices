55c1e760df2a1a5d21c59a5c7bf8656a40a9b547
safakcivelek <safakcivelek3@gmail.com>
2024-05-11 16:10:32 +0300

    Update

--- DersNotuBulma/app.js
+++ DersNotuBulma/app.js
@@ -3,20 +3,26 @@
 // vize1 %30, vize2 %30, final %40
 // Geçme Notu: 70
 
-let vize1 = Number(prompt("Vize 1 notunuzu giriniz :"));
-let vize2 = Number(prompt("Vize 2 notunuzu giriniz :"));
-let final = Number(prompt("Final notunuzu giriniz :"));
+function calculateAverage() {
+    let vize1 = Number(prompt("Vize 1 notunuzu giriniz :"));
+    let vize2 = Number(prompt("Vize 2 notunuzu giriniz :"));
+    let final = Number(prompt("Final notunuzu giriniz :"));
 
-let average = (vize1 * 30/100) + (vize2 * 30/100) + (final * 40/100);
+    let average = (vize1 * 30 / 100) + (vize2 * 30 / 100) + (final * 40 / 100);
 
-let passed = "Tebrikler geçtiniz. :) | ortalama: " + average;
-let notPassed =  "Kaldınız :( | ortalama: " + average;
+    let passed = "Tebrikler geçtiniz. :) | ortalama: " + average;
+    let notPassed = "Kaldınız :( | ortalama: " + average;
 
-if(average >= 70){
-    alert(passed);
-    console.log(passed);
+    if (average >= 70) {
+        alert(passed);
+        console.log(passed);
+    }
+    else {
+        alert(notPassed);
+        console.log(notPassed);
+    }
 }
-else{
-    alert(notPassed);
-    console.log(notPassed);
-}
+//calculateAverage();
+
+
+
