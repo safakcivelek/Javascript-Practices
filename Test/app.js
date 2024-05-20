bd50658b52e3488af9376ec310098da765298694
safakcivelek <safakcivelek3@gmail.com>
2024-05-20 14:46:12 +0300

    Loops : while

--- Test/app.js
+++ Test/app.js
@@ -2,6 +2,7 @@
 
 // Döngüler
 //--------------------------------
+// ---> For
 // 1 den 10'a kadar yazdır.
 // for (let i = 1; i <= 10; i++) {
 //     console.log(i)
@@ -16,17 +17,45 @@
 // console.log("Toplam : ",toplam);
 //--------------------------------
 // 1 den 10' kadar sayıları yan yana topla.
-var sumOfNumbers = 0;
-var numbers ="";
-for (let i = 1; i <= 10; i++) {
-    sumOfNumbers += i;
-    if(i<10)
-        {
-            numbers += i + "+";
-        }else{
-            numbers += i;
-        }
-}
-console.log("Sayılar => ",numbers);
-console.log("Toplam : ",sumOfNumbers);
+// var sumOfNumbers = 0;
+// var numbers ="";
+// for (let i = 1; i <= 10; i++) {
+//     sumOfNumbers += i;
+//     if(i<10)
+//         {
+//             numbers += i + "+";
+//         }else{
+//             numbers += i;
+//         }
+// }
+// console.log("Sayılar => ",numbers);
+// console.log("Toplam : ",sumOfNumbers);
 //--------------------------------
+
+// ---> While
+// 1 den 10'a kadar yazdır.
+// let sayac = 1;
+// while (10 >= sayac) {
+//     console.log("While:",sayac);
+//     sayac++;
+// }
+
+// 1 den 10 a kadar çift ve tekleri yazdır.
+// let sayac = 1;
+// while (sayac <= 10) {
+//     if (sayac % 2 == 0) {
+//         console.log("Çift:",sayac)
+//     }
+//     else{
+//         console.log("Tek:",sayac)
+//     }
+//     sayac++;
+// }
+//
+let sayac =1;
+while (true) {
+    if (sayac == 7) {
+        break;
+    }
+    sayac++;
+}
