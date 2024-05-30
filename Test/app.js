ccdac620148d4a5a91546353f7a7a5672e821962
safakcivelek <safakcivelek3@gmail.com>
2024-05-30 23:43:26 +0300

    Array : functions

--- Test/app.js
+++ Test/app.js
@@ -52,10 +52,37 @@
 //     sayac++;
 // }
 //
-let sayac =1;
-while (true) {
-    if (sayac == 7) {
-        break;
-    }
-    sayac++;
-}
+// let sayac =1;
+// while (true) {
+//     if (sayac == 7) {
+//         break;
+//     }
+//     sayac++;
+// }
+//----------------------------------------
+
+// *** DİZİLER ***
+
+//? Dizinin Metotları
+
+/*
+push      : dizinin sonuna eleman ekler, ayrıca dizinin uzunluğunu döner.
+unshift   : dizinin başına eleman ekler, eleman sayısını geriye döner.
+
+pop       : dizinin sonundan eleman siler, eleman sayısını döner.
+shift     : dizinin başından eleman siler, eleman sayısını döner.
+
+splice(index,incdex): eleman eklemek ve silmek için kullanılır.
+
+toString  : diziyi stringe çevirebiliriz.
+join      : diziyi stringe çevirir. Farkı ise araya eleman ekleyebiliriz.
+
+concat    : dizileri birleştirmek için kullanılır.
+slice(dilim)       : diziyi istenilen yerden bölüp yeni bir dizi oluşturur.
+length    : dizinin uzunluğunu verir.
+reverse   : diziinin elemanlarını ters çevirir.
+split(bölmek)      : belirli bir ifadeye göre bölüp diziye çevirir.
+indexOf   : elemanın index numarasını verir.
+includes  : verilen elemanı içeriyor mu ona bakar. 
+
+*/
